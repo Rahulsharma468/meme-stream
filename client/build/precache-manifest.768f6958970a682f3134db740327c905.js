ab83f3d8a4c2ff4f5acd4bbaa4515a1a91ab73e7
Ashwani Yadav <ashyadavash@gmail.com>
2021-02-10 03:13:02 +0530

    build

---
+++ client/build/precache-manifest.768f6958970a682f3134db740327c905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991d0ef7ae84cb715d781f06c15acc02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c9482d930188d4c3810",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2a2b0daab8367739cddd",
+    "url": "/static/js/2.08b1a387.chunk.js"
+  },
+  {
+    "revision": "5c9482d930188d4c3810",
+    "url": "/static/js/main.99b822c2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
